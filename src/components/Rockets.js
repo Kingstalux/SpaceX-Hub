ff9e20aa167078a48a96cdfed614f68f0fce451d
Kingstalux <kingsleyngu25@gmail.com>
2021-11-23 04:27:44 +0100

    Display rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -3,7 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rockets';
 
 export default function Rockets() {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+
+  const rocketsArray = useSelector((state) => state.rocketsReducer.rockets);
+  const rocketList = rocketsArray.map((rocket) => (
+    <div key={rocket.id} className="rocket-card">
+      <img src={rocket.image} alt="rocket" />
+      <div>
+        <h3>{rocket.name}</h3>
+        <p>{rocket.description}</p>
+        <button type="button" className="rocket-btn">Reserve Rocket</button>
+      </div>
+    </div>
+  ));
 
   return (
     <div className="rocket-container">
