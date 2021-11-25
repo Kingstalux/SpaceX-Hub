45ba2cbfdcc3993d8e7ae2f45a8d6cddd7244e0c
Kingstalux <kingsleyngu25@gmail.com>
2021-11-25 16:33:33 +0100

    Fix linters

--- src/_tests_/Profile.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Profile from '../components/Profile';
-
-it('matches rocket snapshot', () => {
-    const profile = renderer.create(<Profile />).toJSON();
-    expect(profile).toMatchSnapshot();
-})
--- src/_tests_/Rockets.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Rockets from '../components/Rockets';
-
-it('matches rocket snapshot', () => {
-    const tree = renderer.create(<Rockets />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
